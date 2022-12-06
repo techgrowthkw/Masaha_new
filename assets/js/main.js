67654e18fc0f783a29b753e7bed9f199ae0e8d0a
Abdallah Mahmoud <a.ali@techgrowthkw.com>
2022-12-07 01:27:39 +0200

    Services

--- assets/js/main.js
+++ assets/js/main.js
@@ -230,3 +230,15 @@ $(document).ready(function(){
     );
     wow.init()
 });
+// Map
+function myMap() {
+    var mapCanvas = document.getElementById("map");
+    var myCenter = new google.maps.LatLng(29.079721, 48.087246);
+    var mapOptions = { center: myCenter, zoom: 18 };
+    var map = new google.maps.Map(mapCanvas, mapOptions);
+    var marker = new google.maps.Marker({
+        position: myCenter,
+        animation: google.maps.Animation.BOUNCE
+    });
+    marker.setMap(map);
+}
